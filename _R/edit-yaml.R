dd411470a13a40f8d132c2d529f0b9011d2447a0
jepusto <jepusto@gmail.com>
2024-05-22 07:25:16 -0500

    Edited autthors.

--- _R/edit-yaml.R
+++ _R/edit-yaml.R
@@ -3,8 +3,8 @@ library(tidyverse)
 edit_admin <- function(file, replacement = "", multi_replacement = "James E. Pustejovsky") {
   
   readLines(file) |>
-    str_replace("author[s]: admin", replacement) |>
     str_replace("- admin", paste("-", multi_replacement)) |>
+    str_replace("author[s]: admin", replacement) |>
     write_lines(file = file)
 }
 
@@ -15,3 +15,39 @@ list.files(
   recursive = TRUE
 ) |> 
   walk(edit_admin)
+
+list.files(
+  "publication", 
+  pattern = "index.qmd", 
+  full.names = TRUE, 
+  recursive = TRUE
+) |> 
+  walk(
+    edit_admin, 
+    replacement = "authors: \n - James E. Pustejovsky", 
+    multi_replacement = "James E. Pustejovsky"
+  )
+
+list.files(
+  "software", 
+  pattern = "index.qmd", 
+  full.names = TRUE, 
+  recursive = TRUE
+) |> 
+  walk(
+    edit_admin, 
+    replacement = "authors: \n - James E. Pustejovsky", 
+    multi_replacement = "James E. Pustejovsky"
+  )
+
+list.files(
+  "presentations", 
+  pattern = ".qmd", 
+  full.names = TRUE, 
+  recursive = TRUE
+) |> 
+  walk(
+    edit_admin, 
+    replacement = "authors: \n - James E. Pustejovsky", 
+    multi_replacement = "James E. Pustejovsky"
+  )
